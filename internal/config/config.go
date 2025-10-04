8f603f9fdaaf6365baf1597561792c59a812ba0e
Igor Grieder Melo <igorgrieder@gmail.com>
2025-10-04 17:17:29 -0300

    proper envs config

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,16 +9,32 @@ type Config struct {
 	PORT       int
 	REDIS_ADDR string
 	REDIS_PORT int
+	HOST       string
+	PORT_PG    int
+	USER       string
+	DB_NAME    string
+	PG_PASS    string
 }
 
 func NewConfig() *Config {
 	port := parseInt(getEnv("PORT", "8080"))
 	reddisAddr := getEnv("REDIS_ADDR", "redis")
 	reddisPort := parseInt(getEnv("REDIS_PORT", "6379"))
+	host := getEnv("PG_HOST", "localhost")
+	portPG := parseInt(getEnv("PG_PORT", "5432"))
+	user := getEnv("PG_USER", "postgres")
+	dbname := getEnv("PG_DB", "leaky-bucket")
+	pgPass := getEnv("PG_PASS", "none")
+
 	return &Config{
 		PORT:       port,
 		REDIS_ADDR: reddisAddr,
 		REDIS_PORT: reddisPort,
+		HOST:       host,
+		PORT_PG:    portPG,
+		USER:       user,
+		DB_NAME:    dbname,
+		PG_PASS:    pgPass,
 	}
 }
 
