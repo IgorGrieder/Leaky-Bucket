63cc0e1cd223f42d7dffd0a5e89857b1b263f4e0
Igor Grieder Melo <igorgrieder@gmail.com>
2025-10-04 17:03:17 -0300

    .env

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/IgorGrieder/Leaky-Bucket/internal/database"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("ENV loading failed, canceling the program")
+		os.Exit(1)
+	}
+
 	fmt.Println("Starting the program")
 
 	database.StartConns()
--- cmd/presentation/server.go
+++ cmd/presentation/server.go
@@ -1,5 +1,20 @@
 package presentation
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
 func StartHttpServer() {
+
+	mux := http.NewServeMux()
+
+	svr := &http.Server{Addr: fmt.Sprintf(":%d", cfg.PORT), Handler: mux}
+
+	if err := svr.ListenAndServe(); err != nil {
+		fmt.Println("Server crashed for some reason")
+		os.Exit(1)
+	}
 
 }
