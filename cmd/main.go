1a3884f4760d4188a0b917871ba1c050f0fbbc28
Igor Grieder Melo <igorgrieder@gmail.com>
2025-10-05 15:14:27 -0300

    docker-compose and removing the actual env file

--- cmd/main.go
+++ cmd/main.go
@@ -2,32 +2,28 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/IgorGrieder/Leaky-Bucket/cmd/presentation"
 	"github.com/IgorGrieder/Leaky-Bucket/internal/application"
 	"github.com/IgorGrieder/Leaky-Bucket/internal/config"
 	"github.com/IgorGrieder/Leaky-Bucket/internal/database"
 	"github.com/IgorGrieder/Leaky-Bucket/internal/repository"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	cfg := config.NewConfig()
-
-	if err != nil {
-		fmt.Println("ENV loading failed, canceling the program")
-		os.Exit(1)
-	}
-
 	fmt.Println("Starting the program")
 
+	// ENVs
+	cfg := config.NewConfig()
+
+	// Database connections
 	connections := database.StartConns(cfg)
 
+	// Repositories
 	LimitingRepository := repository.NewLimitingRepository(connections.Redis)
 	MutationRepository := repository.NewMutationRepository(connections.PG)
 
+	// Services
 	gatewayService := &application.ProcessorService{
 		MutationRepository: MutationRepository,
 		LimitingRepository: LimitingRepository,
@@ -35,5 +31,6 @@ func main() {
 
 	fmt.Println("Root layer stablished, starting the http server")
 
+	// HTTP Server
 	presentation.StartHttpServer(cfg, gatewayService)
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/IgorGrieder/Leaky-Bucket
 go 1.25.0
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/lib/pq v1.10.9
+	github.com/redis/go-redis/v9 v9.14.0
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	github.com/redis/go-redis/v9 v9.14.0 // indirect
 )
