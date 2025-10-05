b0438d693f582b05b0ccda06d5db2316e17434cc
Igor Grieder Melo <igorgrieder@gmail.com>
2025-10-05 01:00:34 -0300

    geenrate method

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -15,7 +16,7 @@ type JWT struct {
 func Authenticate(tokenString string, hashSecret string) (*jwt.Token, error) {
 	claims := &JWT{}
 
-	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
@@ -24,4 +25,22 @@ func Authenticate(tokenString string, hashSecret string) (*jwt.Token, error) {
 
 	return token, err
 
+}
+
+func GenerateToken(userID, pix string, secretKey []byte) (string, error) {
+	// Create the claims
+	claims := &JWT{
+		UserID: userID,
+		PIX:    pix,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   userID,
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err := token.SignedString(secretKey)
+	return signedToken, err
 }
