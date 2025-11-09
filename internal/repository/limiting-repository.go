32f930ccd7268fb84132925c9b513c6e5128b32c
Igor Grieder Melo <igorgrieder@gmail.com>
2025-11-09 12:08:43 -0300

    consuming changes

--- internal/repository/limiting-repository.go
+++ internal/repository/limiting-repository.go
@@ -22,39 +22,35 @@ func NewLimitingRepository(redis *redis.Client) *LimitingRepository {
 }
 
 func (r *LimitingRepository) TryConsumeToken(ctx context.Context, key string) (bool, error) {
-	ctxRedis, cancel := context.WithTimeout(ctx, 1*time.Second)
-	defer cancel()
+	ctxSet, cancelSet := context.WithTimeout(ctx, 1*time.Second)
+	defer cancelSet()
 
-	remaining, err := r.Redis.Decr(ctxRedis, key).Result()
-
-	if errors.Is(err, redis.Nil) || err != nil {
-		ctxInit, cancelInit := context.WithTimeout(ctx, 1*time.Second)
-		defer cancelInit()
-
-		// SetNX for race conditions
-		wasSet, err := r.Redis.SetNX(ctxInit, key, r.MAX_ATTEMPTS-1, 24*time.Hour).Result()
-		if err != nil {
-			return false, fmt.Errorf("error initializing token bucket: %w", err)
-		}
-
-		if wasSet {
-			return true, nil
-		}
-
-		// if we lost the race create again
-		return r.TryConsumeToken(ctx, key)
+	wasSet, err := r.Redis.SetNX(ctxSet, key, r.MAX_ATTEMPTS-1, 0).Result()
+	if err != nil {
+		return false, fmt.Errorf("error initializing token bucket: %w", err)
 	}
 
-	// If we got negative we must rollback
+	if wasSet {
+		return true, nil
+	}
+
+	ctxDecr, cancelDecr := context.WithTimeout(ctx, 1*time.Second)
+	defer cancelDecr()
+
+	remaining, err := r.Redis.Decr(ctxDecr, key).Result()
+	if err != nil {
+		return false, fmt.Errorf("error decrementing token bucket: %w", err)
+	}
+
 	if remaining < 0 {
 		ctxRollback, cancelRollback := context.WithTimeout(ctx, 1*time.Second)
 		defer cancelRollback()
 
 		r.Redis.Incr(ctxRollback, key)
+
 		return false, nil
 	}
 
-	// Consumed
 	return true, nil
 }
 
@@ -77,4 +73,3 @@ func (r *LimitingRepository) RefillToken(ctx context.Context, key string) error 
 
 	return nil
 }
-
